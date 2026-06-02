77f296f3a1e65460ce52447c1e2a115a3645bf45
diegoruiz2126 <diegoruiz2126@gmail.com>
2026-06-02 09:45:58 +0200

    Clase juego

---
+++ Actividad1/src/main/java/com/mycompany/actividad1/Juego.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.actividad1;
+
+/**
+ *
+ * @author PC-01
+ */
+public class Juego {
+    private String nombre;
+    private boolean digital;
+    private double precio;
+    private String codigojuego;
+    private String[] genero;
+}
